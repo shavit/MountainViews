e0a59814441e4ad26aa59e5b6562120b593e0b50
shavit <shavit@users.noreply.github.com>
2016-09-17 10:02:55 -0400

    🚡

--- lib/mountain_view.ex
+++ lib/mountain_view.ex
@@ -1,2 +1,12 @@
 defmodule MountainView do
+  import Plug.Conn
+
+  def init([]), do: false
+  def call(conn, _opts), do: conn
+
+  def homepage(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Mountain View")
+  end
 end
