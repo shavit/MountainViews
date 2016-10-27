fab3b2db045ebb308c7c09ad188b060afe9afc38
shavit <shavit@users.noreply.github.com>
2016-10-27 11:58:21 -0400

    🚡

--- test/test_grid.py
+++ test/test_grid.py
@@ -4,6 +4,13 @@ from lib.paths import MVGraph
 class GridTest(unittest.TestCase):
     def testDrawing(self):
         graph = MVGraph()
+        graph.edges = {
+            '1': ['2'],
+            '2': ['1','3','4'],
+            '3': ['1'],
+            '4': ['5','1'],
+            '5': ['2']
+        }
 
         self.assertIsNotNone(graph)
         pass
