9730eaf0e372088986746556525aaee773f8c999
shavit <shavit@users.noreply.github.com>
2016-10-28 11:48:31 -0400

    🚡

--- lib/paths/__init__.py
+++ lib/paths/__init__.py
@@ -15,5 +15,16 @@ class MVGraph():
 
         return nodes
 
+    #
+    #        [0,1]
+    #   [-1,0] * [1,0]
+    #        [0,-1]
+    #
+    def simpleNeighbors(self, node):
+        directions = list([0,1],[1,0],[0,-1],[-1,0])
+        res = list()
+        for item in directions:
+            res.append(node[0] + item[0], node[1] + item[1])
+
     def neighbors(self, id):
         return self.edges[id]
