20f9a519f44c33496cac0ac15208478a7599a05e
shavit <shavit@users.noreply.github.com>
2016-11-08 16:42:26 -0500

    🚡

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,11 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+IO.inspect Mix.env
+IO.puts Mix.env
+
+config :mountain_view, [
+  base_url: :os.getenv("BASE_URL"),
+  database_url: :os.getenv("POSTGRESQL_URI")
+]
--- lib/mountain_view.ex
+++ lib/mountain_view.ex
@@ -2,7 +2,24 @@ defmodule MountainView do
   import Plug.Conn
 
   def init([]), do: false
-  def call(conn, _opts), do: conn
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Mountain views")
+  end
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+    port = Application.get_env(:mountain_view, :cowboy_port, 4000)
+    children = [
+      Plug.Adapters.Cowboy.child_spec(:http,
+        MountainView,
+        [],
+        port: port)
+    ]
+    opts = [strategy: :one_for_one, name: MountainView.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 
   def homepage(conn, _opts) do
     conn
