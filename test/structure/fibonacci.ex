089f58781ecead40e184b71778842d082fdff5cf
shavit <shavit@users.noreply.github.com>
2016-11-09 19:12:07 -0500

    🚡

---
+++ test/structure/fibonacci.ex
@@ -0,0 +1,20 @@
+defmodule Fibonacci do
+
+  @doc """
+    a - First number
+    b - Sum of a and the previous a from iterate(a, b, n-1)
+  """
+  def iterate(a, b, n) do
+    cond do
+      n == 0 -> a
+      true -> iterate((a+b), a, n-1)
+    end
+  end
+
+  def call do
+    # Run n times
+    Fibonacci.iterate(1, 0, _n=7)
+  end
+end
+
+IO.inspect Fibonacci.call
