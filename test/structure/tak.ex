4bb87a7a794244a3e23cc411d316fe8d5435f55d
shavit <shavit@users.noreply.github.com>
2016-11-09 21:28:22 -0500

    🚡

---
+++ test/structure/tak.ex
@@ -0,0 +1,20 @@
+defmodule Tak do
+  def tak(x,y,z) do
+    IO.puts "X: #{x}, Y: #{y}, Z: #{z}"
+    if x > y do
+      tak(
+        tak(x-1, y, z),
+        tak(y-1, z, x),
+        tak(z-1, x, y)
+      )
+    else
+      z
+    end
+  end
+
+  def call do
+    tak(30,20,10)
+  end
+end
+
+IO.inspect Tak.call
